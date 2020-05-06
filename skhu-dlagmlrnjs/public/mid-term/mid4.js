4ca0c65494df4d1cbf17a6ab81b4349cc51bbf62
gmlrnjsdla <dlagmlrnjs@gmail.com>
2020-05-06 09:40:27 +0900

    commit

--- skhu-dlagmlrnjs/public/mid-term/mid4.js
+++ skhu-dlagmlrnjs/public/mid-term/mid4.js
@@ -1,7 +1,7 @@
-let a = []; 
+let arr = []; 
 
  for (let i = 0; i < 100; ++i) { 
      a[i] = Math.floor(Math.random() * 100 + 1); 
 } 
-let sum = a.reduce((a,b) => a+b)
+let sum = arr.reduce((a,b) => a+b)
 console.log(sum);
